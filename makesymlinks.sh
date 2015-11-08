5b584669c6277740aaebef8be1700e94c90a03e4
Dmitry Danilich <dmitry.danilich@agileengine.com>
2015-11-08 15:03:13 +0200

    Update 'files' variable

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vim vimrc oh-my-zsh zshrc tmux.conf profile"    # list of files/folders to symlink in homedir
+files="vim vimrc zshrc tmux.conf profile"    # list of files/folders to symlink in homedir
 
 ##########
 

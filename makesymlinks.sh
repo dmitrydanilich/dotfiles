45fc6752c05400042cd291af216ae1a85632065f
Dmitry Danilich <danilichdim@gmail.com>
2016-01-30 15:35:35 +0200

    Add tmuxinator to the files list

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc tmux.conf profile"    # list of files/folders to symlink in homedir
+files="vimrc zshrc tmux.conf profile tmuxinator"    # list of files/folders to symlink in homedir
 
 ##########
 
